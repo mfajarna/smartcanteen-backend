bddfbe7ab07e6d6248a1c57ec09a6ff6c1d73db5
Muhammad Fajar <mfajarna@gmail.com>
2021-10-18 02:13:05 +0700

    First Commit

--- resources/views/layouts/topnav/topnav.blade.php
+++ resources/views/layouts/topnav/topnav.blade.php
@@ -32,16 +32,7 @@
               </li>
             </ul>
           </li>
-          
-          {{-- <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
-            <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
-              <div class="sidenav-toggler-inner">
-                <i class="sidenav-toggler-line"></i>
-                <i class="sidenav-toggler-line"></i>
-                <i class="sidenav-toggler-line"></i>
-              </div>
-            </a>
-          </li> --}}
+        
           <li class="nav-item px-3 d-flex align-items-center">
             <a href="{{ route('profile.show') }}" class="nav-link text-body p-0">
               <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>

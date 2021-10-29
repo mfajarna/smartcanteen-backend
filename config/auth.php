7f379e371b62dcb0bb09e16cdb2e2cd424619607
muhammad fajar <invasionfajar@gmail.com>
2021-10-29 23:15:00 +0700

    API Login and Fetch

--- config/auth.php
+++ config/auth.php
@@ -62,7 +62,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\Tenant\Tenant_m::class,
+            'model' => App\Models\User::class,
         ],
 
         // 'users' => [

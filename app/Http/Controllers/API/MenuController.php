86ffafba19ee58737c609943c4cc9ea8c49ab809
muhammad fajar <invasionfajar@gmail.com>
2021-11-01 04:12:34 +0700

    API Fetch Menu

--- app/Http/Controllers/API/MenuController.php
+++ app/Http/Controllers/API/MenuController.php
@@ -27,7 +27,7 @@ class MenuController extends Controller
             }
             if($status)
             {
-                $model = Menu_m::where('status', $status)->get();
+                $model = Menu_m::where('is_active', $status)->get();
             }
 
             return ResponseFormatter::success($model, 'Berhasil ambil Data Menu');

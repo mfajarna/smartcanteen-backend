87e7fd2a0c8e83ec86ab4d827bed0e685ec663c2
muhammad fajar <invasionfajar@gmail.com>
2021-11-01 03:20:03 +0700

    API Update Menu

--- app/Http/Controllers/API/MenuController.php
+++ app/Http/Controllers/API/MenuController.php
@@ -108,7 +108,7 @@ class MenuController extends Controller
         try{
             $model = Menu_m::find($id);
 
-            $model->each->delete();
+            // $model->each->delete();
 
             return ResponseFormatter::success($model, 'Berhasil Delete Data');
         }catch(Exception $e)

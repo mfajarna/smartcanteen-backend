8417142e5e03eb6a0a1b67cf8da400a261d37926
muhammad fajar <invasionfajar@gmail.com>
2021-11-08 05:45:29 +0700

    API Menu Fetch by Tenant

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -26,7 +26,7 @@
 
 
 </head>
-<body class="g-sidenav-show bg-gray-custom">
+<body class="g-sidenav-show bg-gray-100">
     @include('layouts.sidenav.sidenav')
 
     <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg ">

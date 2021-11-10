ee08dedfc5d687f32ef6b069be820387363ddd2e
muhammad fajar <invasionfajar@gmail.com>
2021-11-10 09:07:36 +0700

    View Tenant

--- resources/views/tenant/index.blade.php
+++ resources/views/tenant/index.blade.php
@@ -29,74 +29,8 @@
             </div>
         </div>
 
-        {{-- @include('modal.tenant.tenant-view') --}}
-        <div class="modal fade" id="ModalView" tabindex="-1" role="dialog" style="z-index: 1050, display: none" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h6 class="modal-title" id="title-tenant"></h6>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Nama Tenant</p>
-                        <h6 class="font-weight-bolder mb-0" id="nama_tenant"> </h6>
-                    </div>
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Nama Pemilik</p>
-                        <h6 class="font-weight-bolder mb-0" id="nama_pemilik"> </h6>
-                    </div>
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">No Telepon</p>
-                        <h6 class="font-weight-bolder mb-0" id="no_telp"> </h6>
-                    </div>
-                </div>
-                <hr>
+        @include('modal.tenant.tenant-view')
 
-                <div class="row">
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Lokasi Kantin</p>
-                        <h6 class="font-weight-bolder mb-0" id="lokasi_kantin"> </h6>
-                    </div>
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Deskripsi Kantin</p>
-                        <h6 class="font-weight-bolder mb-0" id="desc_kantin"> </h6>
-                    </div>
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Jenis Bank</p>
-                        <h6 class="font-weight-bolder mb-0" id="jenis_bank"> </h6>
-                    </div>
-                </div>
-                <hr>
-
-                <div class="row">
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">No Rekening</p>
-                        <h6 class="font-weight-bolder mb-0" id="no_rekening"> </h6>
-                    </div>
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Nama Rekening</p>
-                        <h6 class="font-weight-bolder mb-0" id="nama_rekening"> </h6>
-                    </div>
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Status Tenant</p>
-                        <h6 class="font-weight-bolder mb-0" id="status"> </h6>
-                    </div>
-                </div>
-
-
-
-
-
-
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-link text-success text-gradient px-4 mb-0 mt-2 close-modal" onClick="closeModal()" data-dismiss="modal">Close</button>
-            </div>
-            </div>
-        </div>
-        </div>
 
     @endsection
 

ec2720afaebd1911720a260e8ce433020cd378d6
mfajarna <invasionfajar@gmail.com>
2021-11-10 15:47:39 +0700

    Perbaikan Tenant View

--- resources/views/modal/tenant/tenant-view.blade.php
+++ resources/views/modal/tenant/tenant-view.blade.php
@@ -6,52 +6,50 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <div class="row">
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Nama Tenant</p>
-                        <h6 class="font-weight-bolder mb-0" id="nama_tenant"> </h6>
-                    </div>
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Nama Pemilik</p>
-                        <h6 class="font-weight-bolder mb-0" id="nama_pemilik"> </h6>
-                    </div>
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">No Telepon</p>
-                        <h6 class="font-weight-bolder mb-0" id="no_telp"> </h6>
-                    </div>
-                </div>
-                <hr>
+                <ul class="list-group">
+                    <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
+                        <div class="d-flex flex-column">
+                            <h6 class="mb-2">Informasi Tenant</h6>
 
-                <div class="row">
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Lokasi Kantin</p>
-                        <h6 class="font-weight-bolder mb-0" id="lokasi_kantin"> </h6>
-                    </div>
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Deskripsi Kantin</p>
-                        <h6 class="font-weight-bolder mb-0" id="desc_kantin"> </h6>
-                    </div>
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Jenis Bank</p>
-                        <h6 class="font-weight-bolder mb-0" id="jenis_bank"> </h6>
-                    </div>
-                </div>
-                <hr>
-
-                <div class="row">
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">No Rekening</p>
-                        <h6 class="font-weight-bolder mb-0" id="no_rekening"> </h6>
-                    </div>
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Nama Rekening</p>
-                        <h6 class="font-weight-bolder mb-0" id="nama_rekening"> </h6>
-                    </div>
-                    <div class="col-sm-4">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Status Tenant</p>
-                        <h6 class="font-weight-bolder mb-0" id="status"> </h6>
-                    </div>
-                </div>
+                            <span class="mb-2 text-xs">
+                                Nama Tenant : 
+                                <span class="text-dark font-weight-bold ms-sm-2" id="nama_tenant"></span>
+                            </span>
+                            <span class="mb-2 text-xs">
+                                Nama Pemilik : 
+                                <span class="text-dark font-weight-bold ms-sm-2" id="nama_pemilik"></span>
+                            </span>
+                            <span class="mb-2 text-xs">
+                                No Telepon : 
+                                <span class="text-dark font-weight-bold ms-sm-2" id="no_telp"></span>
+                            </span>
+                            <span class="mb-2 text-xs">
+                                Lokasi Kantin : 
+                                <span class="text-dark font-weight-bold ms-sm-2" id="lokasi_kantin"></span>
+                            </span>
+                            <span class="mb-2 text-xs">
+                                Deskripsi Kantin : 
+                                <span class="text-dark font-weight-bold ms-sm-2" id="desc_kantin"></span>
+                            </span>
+                            <span class="mb-2 text-xs">
+                                Jenis Bank : 
+                                <span class="text-dark font-weight-bold ms-sm-2" id="jenis_bank"></span>
+                            </span>
+                            <span class="mb-2 text-xs">
+                                No Rekening : 
+                                <span class="text-dark font-weight-bold ms-sm-2" id="no_rekening"></span>
+                            </span>
+                            <span class="mb-2 text-xs">
+                                Nama Rekening : 
+                                <span class="text-dark font-weight-bold ms-sm-2" id="nama_rekening"></span>
+                            </span>
+                            <span class="mb-2 text-xs">
+                                Status Tenant : 
+                                <span class="text-dark font-weight-bold ms-sm-2" id="status"></span>
+                            </span>
+                        </div>
+                    </li>
+                </ul>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-link text-success text-gradient px-4 mb-0 mt-2 close-modal" onClick="closeModal()" data-dismiss="modal">Close</button>

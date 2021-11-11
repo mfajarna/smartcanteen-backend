b354dc40b5b75d9ee13aba47bd8a57bb2b5aaf24
muhammad fajar <invasionfajar@gmail.com>
2021-11-11 09:31:02 +0700

    add transaction

---
+++ app/Models/transaction/Transaction_m.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\transaction;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction_m extends Model
+{
+    use HasFactory;
+
+    protected $table ="tb_transactions";
+}
---
+++ database/migrations/2021_11_10_164731_create_transaction_ms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionMsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_menu');
+            $table->integer('id_tenant');
+            $table->string('status');
+            $table->string('method');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_transactions');
+    }
+}

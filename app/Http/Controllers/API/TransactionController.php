13a76dad9a1d7c84e6d283a94a6d1e45f07ff9fc
muhammad fajar <invasionfajar@gmail.com>
2021-12-19 02:18:07 +0700

    ADD API TRANSACTIONS

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -71,10 +71,9 @@ class TransactionController extends Controller
             $status = $request->input('status');
             $id_tenant = $request->input('id_tenant');
 
-            $model = Transaction_m::with(['menu','tenant'])
+            $model = Transaction_m::with('menu','tenant')
                                  ->where('id_tenant', $id_tenant)
                                  ->where('status', $status)
-                                 ->orderBy('created_at', 'DESC')
                                  ->get();
 
             return ResponseFormatter::success($model,'Berhasil Mengambil Pesanan Order');

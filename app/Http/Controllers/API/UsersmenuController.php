f58499e4b390b73328b03d31b2ab5fe52334abe4
muhammad fajar <invasionfajar@gmail.com>
2022-02-01 17:41:51 +0700

    Fix API transaction

--- app/Http/Controllers/API/UsersmenuController.php
+++ app/Http/Controllers/API/UsersmenuController.php
@@ -202,7 +202,7 @@ class UsersmenuController extends Controller
                         ->select(
                             'tb_tenant.nama_tenant',
                             'tb_tenant.desc_kantin',
-                            'tb_transactions.quantity',
+                            DB::raw('SUM(tb_transactions.quantity)'),
                             'tb_tenant.profile_photo_path'
                         )
                         ->groupBy('tb_tenant.nama_tenant')

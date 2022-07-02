f5a99e1d22bd05e74e160c21495ec8abe8b2912b
mfajarna <invasionfajar@gmail.com>
2022-07-02 12:08:00 +0700

    adding param data payload to notifikasi

--- app/Http/Controllers/API/QrisController.php
+++ app/Http/Controllers/API/QrisController.php
@@ -36,8 +36,6 @@ class QrisController extends Controller
             $model->terminal_label = $decode->terminal_label;
             $model->save();
 
-
-
             // Adding update status field to transactions
             $modelTransactions = Transaction_m::where('total_order', $decode->amount)
                                                 ->where('status', 'PENDING')
@@ -50,15 +48,11 @@ class QrisController extends Controller
             ->where('status', 'PENDING')
             ->first();
 
-            
-            
             // get id user from transactions table
             $id_user = $transactions->id_user;
             
             // get kode_transaksi from transactions table
             $kode_transaksi = $transactions->kode_transaksi;
-
-
 
             // Collection User APK
             $user_apk = UserApk::findOrFail($id_user);
@@ -81,14 +75,36 @@ class QrisController extends Controller
               "to": "'.$device_token.'",
               
                 "data" : {
-                    "type" : "Setting"
+                    "kode_transaksi"    : '.$kode_transaksi.',
+                    "id_user"           : '.$id_user.',   
+                    "nama_pelanggan"    : '.$transactions->nama_pelanggan.',
+                    "nim"               : '.$transactions->nim.',        
+                    "id_menu"           : '.$transactions->id_menu.',    
+                    "id_tenant"         : '.$transactions->id_tenant.',
+                    "status"            : '.$status.',
+                    "method"            : '.$transactions->pending.',
+                    "quantity"          : '.$transactions->quantity.',
+                    "created_at"        : '.$transactions->created_at.',
+                    "total"             : '.$transactions->total.',
+                    "tax"               : '.$transactions->tax.',
+                    "kode_uniq"         : '.$transactions->kode_uniq.',
+                    "catatan"           : '.$transactions->catatan.',
+                    "is_cash"           : '.$transactions->is_cash.',
+                    "phoneNumber"       : '.$transactions->phoneNumber.',
+                    "no_table"          : '.$transactions->no_table.',
+                    "total_order"       : '.$transactions->total_order.',
+                    "lokasi_kantin"     : '.$transactions->lokasi_kantin.',
+                    "device_token"      : '.$transactions->device_token.',
+                    "nama_tenant"       : '.$transactions->nama_tenant.',
+                    "nama_tenant"       : '.$transactions->nama_tenant.',
+                    "orderView"         : '. true .',
                 },
                 "collapse_key": "type_a",
                 "priority" : "high",
                 "notification":{
                     "android_channel_id": "default-channel-id",
                     "title": "Pembayaran Qris SmartCanteen",
-                    "body": "Pelanggan dengan kode_transaksi '.$kode_transaksi.' telah melakukan transaksi dengan status '.$status.'"
+                    "body": "Pelanggan dengan kode_transaksi '.$kode_transaksi.' telah melakukan pembayaran dengan status '.$status.'"
                  }
             
             
@@ -104,9 +120,6 @@ class QrisController extends Controller
             
             curl_close($curl);
             // echo $response;
-
-
-                                                
 
             if($model && $modelTransactions && $response)
             {
